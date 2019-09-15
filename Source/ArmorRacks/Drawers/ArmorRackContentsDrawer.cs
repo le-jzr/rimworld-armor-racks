e84db4f125e179336276e3b906f071f194c6b5e1
Kirill <khamenman@gmail.com>
2019-09-15 14:09:05 +0400

    Initial commit

---
+++ Source/ArmorRacks/Drawers/ArmorRackContentsDrawer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ArmorRacks.Things;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ArmorRacks.Drawers
+{
+    public class ArmorRackContentsDrawer
+    {
+        public readonly ArmorRack ArmorRack;
+        public List<ApparelGraphicRecord> ApparelGraphics;
+
+        public ArmorRackContentsDrawer(ArmorRack armorRack)
+        {
+            ArmorRack = armorRack;
+            ApparelGraphics = new List<ApparelGraphicRecord>();
+        }
+
+        public Vector3 BaseHeadOffsetAt(Rot4 rotation)
+        {
+            Vector2 headOffset = ArmorRack.BodyTypeDef.headOffset;
+            switch (rotation.AsInt)
+            {
+                case 0:
+                    return new Vector3(0.0f, 0.0f, headOffset.y);
+                case 1:
+                    return new Vector3(headOffset.x, 0.0f, headOffset.y);
+                case 2:
+                    return new Vector3(0.0f, 0.0f, headOffset.y);
+                case 3:
+                    return new Vector3(-headOffset.x, 0.0f, headOffset.y);
+                default:
+                    return Vector3.zero;
+            }
+        }
+
+        public void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            if (ApparelGraphics.Count != ArmorRack.GetStoredApparel().Count())
+            {
+                ResolveApparelGraphics();
+            }
+            
+            const float angle = 0.0f;
+            Quaternion quaternion = Quaternion.AngleAxis(angle, Vector3.up);
+            Rot4 bodyFacing = ArmorRack.Rotation;
+            Vector3 rootLoc = drawLoc;
+            Vector3 vector3_1 = rootLoc;
+            Vector3 vector3_2 = rootLoc;
+            if (bodyFacing != Rot4.North)
+            {
+                vector3_2.y += 7f / 256f;
+                vector3_1.y += 3f / 128f;
+            }
+            else
+            {
+                vector3_2.y += 3f / 128f;
+                vector3_1.y += 7f / 256f;
+            }
+
+            Vector3 vector3_3 = quaternion * BaseHeadOffsetAt(bodyFacing);
+            Vector3 loc1 = rootLoc + vector3_3;
+            loc1.y += 1f / 32f;
+            Mesh mesh = new GraphicMeshSet(1.5f).MeshAt(bodyFacing);
+
+            for (int index = 0; index < ApparelGraphics.Count; ++index)
+            {
+                if (ApparelGraphics[index].sourceApparel.def.apparel.LastLayer == ApparelLayerDefOf.Overhead)
+                {
+                    if (!ApparelGraphics[index].sourceApparel.def.apparel.hatRenderedFrontOfFace)
+                    {
+                        Material mat = ApparelGraphics[index].graphic.MatAt(bodyFacing);
+                        GenDraw.DrawMeshNowOrLater(mesh, loc1, quaternion, mat, false);
+                    }
+                    else
+                    {
+                        Material mat = ApparelGraphics[index].graphic.MatAt(bodyFacing);
+                        Vector3 loc2 = rootLoc + vector3_3;
+                        loc2.y += !(bodyFacing == Rot4.North) ? 9f / 256f : 1f / 256f;
+                        GenDraw.DrawMeshNowOrLater(mesh, loc2, quaternion, mat, false);
+                    }
+                }
+                else
+                {
+                    Material mat = ApparelGraphics[index].graphic.MatAt(bodyFacing);
+                    GenDraw.DrawMeshNowOrLater(mesh, vector3_1, quaternion, mat, false);
+                }
+            }
+
+            foreach (Apparel apparel in ArmorRack.GetStoredApparel())
+            {
+                apparel.DrawWornExtras();
+            }
+        }
+
+        public void ResolveApparelGraphics()
+        {
+            ApparelGraphics.Clear();
+            foreach (Apparel apparel in ArmorRack.GetStoredApparel())
+            {
+                ApparelGraphicRecord rec;
+                if (ApparelGraphicRecordGetter.TryGetGraphicApparel(apparel, ArmorRack.BodyTypeDef, out rec))
+                    ApparelGraphics.Add(rec);
+            }
+        }
+    }
+}
---
+++ Source/ArmorRacks/Things/ArmorRack.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using ArmorRacks.Drawers;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ArmorRacks.Things
+{
+    public class ArmorRack : Building, IHaulDestination, IThingHolder
+    {
+        public StorageSettings Settings;
+        public ThingOwner<Thing> InnerContainer;
+        public ArmorRackContentsDrawer ContentsDrawer;
+        public BodyDef BodyDef => BodyDefOf.Human;
+        public BodyTypeDef BodyTypeDef => BodyTypeDefOf.Male;
+        public bool StorageTabVisible => true;
+
+        public ArmorRack()
+        {
+            InnerContainer = new ThingOwner<Thing>(this, false);
+            ContentsDrawer = new ArmorRackContentsDrawer(this);
+        }
+
+        public StorageSettings GetStoreSettings()
+        {
+            return Settings;
+        }
+
+        public StorageSettings GetParentStoreSettings()
+        {
+            return def.building.fixedStorageSettings;
+        }
+
+        public override void PostMake()
+        {
+            base.PostMake();
+            Settings = new StorageSettings(this);
+            if (def.building.defaultStorageSettings == null)
+                return;
+            Settings.CopyFrom(def.building.defaultStorageSettings);
+        }
+
+        public bool Accepts(Thing t)
+        {
+            bool result = Settings.AllowedToAccept(t);
+            if (result)
+            {
+                if (t.def.IsWeapon)
+                {
+                    result = CanStoreWeapon(t);
+                }
+                else if (t.def.IsApparel)
+                {
+                    result = CanStoreApparel((Apparel) t);
+                }
+            }
+            return result;
+        }
+
+        public bool CanStoreWeapon(Thing weapon)
+        {
+            foreach (Thing storedThing in GetDirectlyHeldThings())
+            {
+                if (storedThing.def.IsWeapon)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool CanStoreApparel(Apparel apparel)
+        {
+            foreach (Apparel storedApparel in GetStoredApparel())
+            {
+                if (!ApparelUtility.CanWearTogether(storedApparel.def, apparel.def, BodyDef))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public IEnumerable<Apparel> GetStoredApparel()
+        {
+            foreach (Thing storedThing in GetDirectlyHeldThings())
+            {
+                if (storedThing.def.IsApparel)
+                {
+                    yield return (Apparel) storedThing;
+                }
+            }
+        }
+
+        public void GetChildHolders(List<IThingHolder> outChildren)
+        {
+            ThingOwnerUtility.AppendThingHoldersFromThings(outChildren, GetDirectlyHeldThings());
+        }
+
+        public ThingOwner GetDirectlyHeldThings()
+        {
+            return InnerContainer;
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Deep.Look(ref InnerContainer, "InnerContainer", this);
+        }
+
+        public override void Draw()
+        {
+            this.DrawAt(DrawPos);
+        }
+
+        public override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            base.DrawAt(drawLoc, flip);
+            ContentsDrawer.DrawAt(drawLoc, flip);
+        }
+
+        public void DropApparel()
+        {
+            IntVec3 dropPos = new IntVec3(DrawPos);
+            InnerContainer.TryDropAll(dropPos, Map, ThingPlaceMode.Near);
+        }
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            DropApparel();
+            base.Destroy(mode);
+        }
+    }
+}
